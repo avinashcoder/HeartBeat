8e1e882fc1d96dc68b648f9f701a5aaf96e57d36
Avinash Kumar Singh <kumaravinash328@gmail.com>
2019-03-10 11:57:20 -0700

    Initial Commit

--- app/src/main/java/com/heartbeat/nyinst/avinash/RegistrationNameActivity.java
+++ app/src/main/java/com/heartbeat/nyinst/avinash/RegistrationNameActivity.java
@@ -1,19 +1,41 @@
 package com.heartbeat.nyinst.avinash;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class RegistrationNameActivity extends AppCompatActivity {
 
     Button bt_reg_name_next;
+    ImageView profile_pic,add_profile_pic;
+    private final int IMG_REQUEST = 1;
+    private Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_name);
+
+        profile_pic=(ImageView)findViewById(R.id.reg_profile_pic);
+        add_profile_pic=(ImageView)findViewById(R.id.reg_add_profile_pic);
         bt_reg_name_next=(Button)findViewById(R.id.btn_reg_name_next);
+
+        add_profile_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
         bt_reg_name_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +43,30 @@ public class RegistrationNameActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void selectImage()
+    {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMG_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==IMG_REQUEST && resultCode==RESULT_OK && data!=null)
+        {
+            Uri path = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), path);
+                profile_pic.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
