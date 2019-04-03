c3f4f59eb5c8ad3147a1e77c3607453887dca01e
Avinash Kumar Singh <kumaravinash328@gmail.com>
2019-04-03 16:54:30 +0530

    Shared Preferences for Registration Part is done

--- app/src/main/java/com/lifeline/nyinst/avinash/HomeActivity.java
+++ app/src/main/java/com/lifeline/nyinst/avinash/HomeActivity.java
@@ -1,21 +1,29 @@
 package com.lifeline.nyinst.avinash;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import static com.lifeline.nyinst.avinash.SplashActivity.addressFinal;
 import static com.lifeline.nyinst.avinash.SplashActivity.bloodGroupFinal;
@@ -30,23 +38,85 @@ public class HomeActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
-    ImageView profilepic,bloodGroup;
+    ImageView profileImgView,bloodGroupImgView;
     TextView userName,address;
     SharedPreferences sharedPreferences;
-    String name,addressvalue;
+    String name,addressvalue,contact,bloodGroup;
+    ImageView camSelectImage,imgPost;
+    Button btnPost;
+    EditText editTextPost;
+    String postText;
+    ProgressBar progressBar;
+
+
+    private final int IMG_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
         userName = headerView.findViewById(R.id.nav_drawer_user_name);
         address=headerView.findViewById(R.id.nav_drawer_user_address);
+        profileImgView=headerView.findViewById(R.id.nav_drawer_user_profile_pic);
+        bloodGroupImgView=headerView.findViewById(R.id.nav_drawer_user_blood_group);
+
+        camSelectImage=findViewById(R.id.home_cam_image_select);
+        imgPost=findViewById(R.id.home_post_image);
+        editTextPost=findViewById(R.id.home_edit_text_post);
+        btnPost=findViewById(R.id.home_post_btn);
+        progressBar=findViewById(R.id.home_post_upload_progressBar);
 
         setUpToolBar();
         retriveSharedPreferenceData();
-        //updateUI();
 
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        imgPost.setVisibility(View.INVISIBLE);
+                        btnPost.setVisibility(View.INVISIBLE);
+                        editTextPost.setText("");
+                    }
+                }, 3000);
+            }
+        });
+
+        camSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        editTextPost.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                postText=editTextPost.getText().toString().trim();
+                if(postText.isEmpty()){
+                    btnPost.setVisibility(View.GONE);
+                }
+                else{
+                    btnPost.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
@@ -71,30 +141,104 @@ public class HomeActivity extends AppCompatActivity {
     private void retriveSharedPreferenceData(){
         sharedPreferences=getSharedPreferences(myPreferences, Context.MODE_PRIVATE);
         if(sharedPreferences.contains(nameFinal)){
-//            userName.setText(sharedPreferences.getString(nameFinal,"User Name"));
-//            address.setText(sharedPreferences.getString(addressFinal,"Address"));
             name=sharedPreferences.getString(nameFinal,"User Name");
-            Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
             userName.setText(name);
         }
         if(sharedPreferences.contains(addressFinal)){
             addressvalue=sharedPreferences.getString(addressFinal,"Address");
-            Toast.makeText(getApplicationContext(),addressvalue,Toast.LENGTH_LONG).show();
             address.setText(addressvalue);
         }
-        if(sharedPreferences.contains(dobFinal)){
-            Toast.makeText(getApplicationContext(),"DOB="+sharedPreferences.getString(dobFinal,"DOB"),Toast.LENGTH_LONG).show();
-        }
         if(sharedPreferences.contains(contactNumberFinal)){
-            Toast.makeText(getApplicationContext(),"Contact="+sharedPreferences.getString(contactNumberFinal,"Contact"),Toast.LENGTH_LONG).show();
+            contact=sharedPreferences.getString(contactNumberFinal,"Contact");
         }
         if(sharedPreferences.contains(bloodGroupFinal)){
-            Toast.makeText(getApplicationContext(),"Blood Group="+sharedPreferences.getString(bloodGroupFinal,"Blood Group"),Toast.LENGTH_LONG).show();
-        }
-        if(sharedPreferences.contains(genderFinal)){
-            Toast.makeText(getApplicationContext(),"Gender="+sharedPreferences.getString(genderFinal,"Gender"),Toast.LENGTH_LONG).show();
+            bloodGroup=sharedPreferences.getString(bloodGroupFinal,"Blood Group");
+            setBloodGroupImage();
         }
     }
 
+    private void setBloodGroupImage(){
+        switch (bloodGroup){
+            case "A+":
+                bloodGroupImgView.setImageResource(R.drawable.a_positive);
+                break;
 
+            case "B+":
+                bloodGroupImgView.setImageResource(R.drawable.b_positive);
+                break;
+
+            case "AB+":
+                bloodGroupImgView.setImageResource(R.drawable.ab_positive);
+                break;
+
+            case "O+":
+                bloodGroupImgView.setImageResource(R.drawable.o_positive);
+                break;
+
+            case "A-":
+                bloodGroupImgView.setImageResource(R.drawable.a_negative);
+                break;
+
+            case "B-":
+                bloodGroupImgView.setImageResource(R.drawable.b_negative);
+                break;
+
+            case "AB-":
+                bloodGroupImgView.setImageResource(R.drawable.ab_negative);
+                break;
+
+            case "O-":
+                bloodGroupImgView.setImageResource(R.drawable.o_negative);
+                break;
+
+            default:
+                bloodGroupImgView.setImageResource(R.drawable.a_positive);
+        }
+    }
+
+    private void selectImage()
+    {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMG_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==IMG_REQUEST && resultCode==RESULT_OK && data!=null)
+        {
+            Uri path = data.getData();
+            imgPost.setImageURI(path);
+            imgPost.setVisibility(View.VISIBLE);
+            btnPost.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+    }
 }
